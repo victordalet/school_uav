523e6f891d5731687305668d744648efd5b6e3cf
victordalet <victor.dalet@protonmail.com>
2024-06-24 03:16:30 +0200

    project_init

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # school_uav
+
+---
+
+## 1. Discover the environment
+
+---
+
+
+## 2. Create env file
+
+---
+
+
+## 3. Autonomous control 
+
+---
+
+### 3.1 - Display map to get coordinates
+
+---
+
+### 3.2 - Configure number and location
+
+---
+
+### 3.3 - Start the mission
+
+
+---
+
+## 4. Reconnaissance mission
+
+---
---
+++ src/discoverer_map.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import torch
+
+
+class DiscovererMap:
+
+    def __init__(self):
+        self.map = {}
+        self.continue_discovery = True
+        self.model_type = "MiDaS_small"
+
+        self.midas = torch.hub.load("intel-isl/MiDaS", self.model_type)
+
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.midas.to(self.device)
+        self.midas.eval()
+
+        self.midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+        if self.model_type == "DPT_Large" or self.model_type == "DPT_Hybrid":
+            self.transform = self.midas_transforms.dpt_transform
+        else:
+            self.transform = self.midas_transforms.small_transform
+
+    def stop_discovery(self):
+        self.continue_discovery = False
+
+    def discover(self, frame: cv2.typing.MatLike):
+        depth_map = self.transform_depth_map(frame)
+
+    def transform_depth_map(self, frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        input_batch = self.transform(img).to(self.device)
+
+        with torch.no_grad():
+            prediction = self.midas(input_batch)
+
+            prediction = torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=img.shape[:2],
+                mode="bicubic",
+                align_corners=False,
+            ).squeeze()
+
+        depth_map = prediction.cpu().numpy()
+
+        depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
+
+        depth_map = (depth_map * 255).astype(np.uint8)
+        depth_map = cv2.applyColorMap(depth_map, cv2.COLORMAP_MAGMA)
+        return depth_map
